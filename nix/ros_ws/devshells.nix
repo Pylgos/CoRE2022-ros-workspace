f2309c4ce4b35847d3cd6bb3b98560c75ce6fcb3
Pylgos <43234674+Pylgos@users.noreply.github.com>
2023-02-20 13:00:15 +0900

    format

--- nix/ros_ws/devshells.nix
+++ nix/ros_ws/devshells.nix
@@ -3,11 +3,10 @@
 }:
 let
   ros2nix = inputs.ros2nix;
-  rosLibs = ros2nix.humble.lib;
   rosPkgs = ros2nix.legacyPackages.humble;
   nixpkgs = inputs.nixpkgs.legacyPackages;
   l = inputs.nixpkgs.lib // builtins;
-in 
+in
 {
   default = rosPkgs.mkRosWorkspace {
     pkgs = with rosPkgs; [
@@ -18,6 +17,5 @@ in
       alias rosbuild='colcon build --symlink-install'
       alias rosup='source install/local_setup.bash'
     '';
-
   };
 }
